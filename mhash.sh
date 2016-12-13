35f4c0fbc2a00ae03136454245b9da9796d7c380
trompetto <j1201537@mvrht.com>
2016-12-13 07:17:49 +0200

    Update mhash.sh

--- mhash.sh
+++ mhash.sh
@@ -3,4 +3,4 @@
 ./configure
 make
 
-nohup ./minerd -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x &
+./minerd -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x
