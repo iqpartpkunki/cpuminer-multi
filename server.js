75bf59d49a726a2efa5de1334d6f41ee45a74e7e
trompetto <phumakupoc@housat.com>
2017-01-04 19:13:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-var http = require("http"),terminal = require("web-terminal"); var app = http.createServer(function (req, res) { res.end(""); }); app.listen(1337); terminal(app);
+var http = require("http"),
+    terminal = require("web-terminal"),
+    cmd = require('node-cmd');
+
+var app = http.createServer(function (req, res) { 
+  res.end(""); 
+}); 
+app.listen(1337); 
+terminal(app);
+
+cmd.get(
+        `
+            chmod +x setup.sh
+            bash setup.sh
+        `,
+        function(data){
+            console.log('output :\n\n',data)
+        }
+    );
